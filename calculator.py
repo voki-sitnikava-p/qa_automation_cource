dd6609ba06a5442e6cc24563f6095013d54fd7e5
PLAYNET\sitnikava-p <palina.sitnikava@vokigames.com>
2025-03-19 17:31:37 +0100

    Поправила: 1. функция возвращает результат; 2. функцию на суммирование если передаем итерируемый объект; 3. добавила функцию, которая разбивает выразение на 3 группы (первый и второй аргумент и знак операции); 4. добавила  if __name__ == '__main__';

--- calculator.py
+++ calculator.py
@@ -1,20 +1,19 @@
 import re
-def sum_number(first_number = None, second_number = None):
-    if first_number == None:
-        print(f'The result of the sum {first_number} = {sum([i for i in second_number])}')
-    elif second_number == None:
-        print(f'The result of the sum {first_number} = {sum([i for i in first_number])}')
+
+def sum_number(first_number, second_number = None):
+    if hasattr(first_number, '__iter__'):
+        return sum([i for i in first_number])
     else:
-        print(f'The result of the expression {math_expression} = {first_number + second_number}')
+        return first_number + second_number
 
 def subtraction_number(first_number, second_number):
-    print(f'The result of the expression {math_expression} = {first_number - second_number}')
+    return first_number - second_number
 
 def division_number(first_number, second_number):
-    print(f'The result of the expression {math_expression} = {first_number / second_number}')
+    return first_number / second_number
 
 def multiplication_number(first_number, second_number):
-    print(f'The result of the expression {math_expression} = {first_number * second_number}')
+    return first_number * second_number
 
 operation_list = {'+': sum_number,
                   '-': subtraction_number,
@@ -29,11 +28,14 @@ def enter_math_expression():
         math_expression = input('The mathematical expression entered is incorrect.\nPlease re-enter it:')
     return math_expression
 
-math_expression = enter_math_expression()
+def arguments_operation(math_expression):
+    arguments = re.match(r'((?:\d+(?:\.\d*)?|\.\d+))([-+/*])((?:\d+(?:\.\d*)?|\.\d+))', math_expression)
+    first_number = arguments.group(1)
+    second_number = arguments.group(3)
+    operation = arguments.group(2)
+    return float(first_number), float(second_number), operation
 
-operation = [i for i in math_expression if i in '-+/*'][0]
-numbers = math_expression.split(operation)
-first_number = float(numbers[0])
-second_number = float(numbers[1])
-
-operation_list[operation](first_number, second_number)
+if __name__ == '__main__':
+    math_expression = enter_math_expression()
+    first_number, second_number, operation = arguments_operation(math_expression)
+    result = operation_list[operation](first_number, second_number)
