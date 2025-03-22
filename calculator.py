999a54b5c05231413af69724e52d53ffe49cc618
PLAYNET\sitnikava-p <palina.sitnikava@vokigames.com>
2025-03-22 15:57:06 +0100

    Проверочное задание: Обработка ошибок и исключения

---
+++ calculator.py
@@ -0,0 +1,14 @@
+from mymodule import BasicCalc
+from mymodule import New_calc
+
+#if __name__ == '__main__':
+#    result = BasicCalc.calculate_user_input()
+#    print(result)
+#    first_number = BasicCalc.sum_number([33, 4, 66])
+#    print(first_number)
+
+if __name__ == '__main__':
+   result = New_calc.calculate_user_input()
+   print(result)
+first_number = 4
+second_number = 2
